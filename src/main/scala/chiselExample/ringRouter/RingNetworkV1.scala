c048d8a400d397c72491c2b617442ef1a6b84d9d
allen <Allen-Semifive>
2022-06-15 14:44:04 +0900

    runnable1.

--- src/main/scala/chiselExample/ringRouter/RingNetworkV1.scala
+++ src/main/scala/chiselExample/ringRouter/RingNetworkV1.scala
@@ -14,17 +14,41 @@ class RingRouterV1[T <: chisel3.Data](p: RingNetworkParams[T], id: Int) extends 
   }
 
   val io: RingRouterV1Bundle = IO(new RingRouterV1Bundle)
-  val forMe: Bool = (io.in.bits.addr === id.U) && io.in.valid
 
-  io.in <> io.out
+  io.host.in.ready := true.B
+  io.host.out.bits.addr := 5.U
+  io.host.out.bits.data := 5.U
+  io.host.out.valid := true.B
 
-  io.host.in.ready := io.out.ready
-  io.host.out.valid := forMe
-  io.host.out.bits := io.in.bits
   io.in.ready := io.host.out.ready && io.out.ready
-  io.out.valid := (io.in.fire && !forMe) || io.host.in.fire
-  io.out.bits := Mux(io.host.in.fire, io.host.in.bits, io.in.bits)
+
+  io.out.bits.addr := 5.U
+  io.out.bits.data := 5.U
+  io.out.valid := true.B
+
+
 }
+
+
+//  val forMe: Bool = (io.in.bits.addr === id.U) && io.in.valid
+//
+//  val bufferValid: Bool = Reg(Bool())
+//  val bufferBits: RingMessage[T] = Reg(new RingMessage(p))
+//
+//  io.host.in.ready := io.out.ready
+//  io.host.out.valid := forMe
+//  io.host.out.bits := io.in.bits
+//
+//  io.in.ready := io.host.out.ready && io.out.ready
+//
+////  io.out.valid := (io.in.fire && !forMe) || io.host.in.fire
+////  io.out.bits := Mux(io.host.in.fire, io.host.in.bits, io.in.bits)
+//  bufferValid := (io.in.fire && !forMe) || io.host.in.fire
+//  bufferBits := Mux(io.host.in.fire, io.host.in.bits, io.in.bits)
+//  io.out.valid := bufferValid
+//  io.out.bits := bufferBits
+//
+//  io.in <> io.out
 
 class RingNetworkV1[T <: chisel3.Data](p: RingNetworkParams[T]) extends Module {
 
@@ -35,29 +59,34 @@ class RingNetworkV1[T <: chisel3.Data](p: RingNetworkParams[T]) extends Module {
   val io: RingNetworkV1Port = IO(new RingNetworkV1Port())
   val routers: Seq[RingRouterV1[T]] = Seq.tabulate(p.numHosts){ id => Module(new RingRouterV1(p, id))}
 
-  routers(0).io.host.in.valid := io.ports(0).in.valid
-  routers(0).io.host.in.bits := io.ports(0).in.bits
+//  routers(0).io.in <> io.ports(0).in
+//  routers(1).io.in <> io.ports(1).in
+//  routers(2).io.in <> io.ports(2).in
 
-  routers(0).io.in <> io.ports(0).in
+  routers(0).io.in <> routers(1).io.out
+  routers(1).io.in <> routers(0).io.out
+
   routers(0).io.out <> io.ports(0).out
-
-  routers(1).io.in <> io.ports(1).in
   routers(1).io.out <> io.ports(1).out
+//  routers(2).io.out <> io.ports(2).out
 
   routers(0).io.host.in <> io.ports(0).in
-  routers(0).io.host.out <> io.ports(0).out
-
   routers(1).io.host.in <> io.ports(1).in
-  routers(1).io.host.out <> io.ports(1).out
+//  routers(2).io.host.in <> io.ports(2).in
 
-  //위와 아래를 비교.
-  routers.foldLeft(routers.last){
-    (prev, curr) => prev.io.out <> curr.io.in
-      curr
-  }
-  routers.zip(io.ports).foreach {
-    case (router, port) => router.io.host <> port
-  }
+  routers(0).io.host.out <> io.ports(0).out
+  routers(1).io.host.out <> io.ports(1).out
+//  routers(2).io.host.out <> io.ports(2).out
+
+//  //위와 아래를 비교.
+//  routers.foldLeft(routers.last){
+//    (prev, curr) => prev.io.out <> curr.io.in
+//      curr
+//  }
+//
+//  routers.zip(io.ports).foreach {
+//    case (router, port) => router.io.host <> port
+//  }
 
 }
 
@@ -65,8 +94,7 @@ class RingNetworkV1[T <: chisel3.Data](p: RingNetworkParams[T]) extends Module {
 object RingNetworkV1 extends App {
 
 //  generating(new RingRouterV1(RingNetworkParams(2, UInt(5.W)), 0))
-  generating(new RingNetworkV1(RingNetworkParams(3, UInt(5.W))))
-
+  generating(new RingNetworkV1(RingNetworkParams(2, UInt(5.W))))
 
 }
 
